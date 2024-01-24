d2096345ef685e1800e2807698a7583b5471efa1
n0xstella <bedkent1990@gmail.com>
2024-01-23 22:17:56 -0500

    first commit

---
+++ functools_reduce.py
@@ -0,0 +1,9 @@
+import functools
+
+
+def accumulate(doc, sentence):
+    return ''.join(map(str, sentence + f'{doc}. '))
+    
+
+def accumulate_first_sentences(sentences, n):
+    return functools.reduce(lambda acc, current: accumulate(current, acc), sentences[:n], '')
